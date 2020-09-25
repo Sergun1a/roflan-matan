0cf18736c62293d7966d40773fed088790035527
Sergey Alaev <webest468584>
2020-09-25 18:14:35 +0300

    Heroku updates

--- config/db.php
+++ config/db.php
@@ -1,14 +1,16 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=matan',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+$url = parse_url(getenv("mysql://bb9b4ce32852d9:6fcfc4f3@eu-cdbr-west-03.cleardb.net/heroku_8b4745719d70df9?reconnect=true"));
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+$server   = 'eu-cdbr-west-03.cleardb.net:3306';
+$username = 'bb9b4ce32852d9';
+$password = '6fcfc4f3';
+$db       = 'heroku_8b4745719d70df9';
+
+return [
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=' . $server . ';dbname=' . $db,
+    'username' => $username,
+    'password' => $password,
+    'charset'  => 'utf8',
 ];
